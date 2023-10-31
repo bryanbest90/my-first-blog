4804e692af9751e725d2a89cf0b028ba2d400aa3
Bryan <bmdeodato@gmail.com>
2023-10-31 16:47:49 -0300

    My Django app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6chw89ck^^kaxun__(5pmihf9@d4rj7n)g!mk*niqhlop=qlz&'
+SECRET_KEY = 'django-insecure-42gu9(!cey0hvrs&bn@et-zt1i=m)1sya9we)k5auz=e^v_dt-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
